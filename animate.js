a6f4065b4944a27da7f2b997bc1e0155dbb7c1d7
Alessandro Cartegni <acartegni@gmail.com>
2018-02-09 00:35:25 -0500

    reworked but still issues

--- animate.js
+++ animate.js
@@ -1,20 +1,18 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
+
 var toggle = false;
-var line = false;
 var x = c.offsetX;
 var y = c.offsetY;
 var xdir = 1;
 var ydir = 1;
+var id = 0;
 
 var toggled = document.getElementById("toggle");
 var stopped = document.getElementById("stop");
 
-var stopper = function(e){
-    e.preventDefault();
-    ctx.closePath();
-    ctx.beginPath();
-    ctx.clearRect(0,0,600,600);
+var stopper = function(){
+    window.cancelAnimationFrame(id);
 }
 
 var toggler = function(e){
@@ -26,50 +24,39 @@ var toggler = function(e){
     }
 }
 
-var liner = function(e){
-    if(line){
-	line=false;
+var bounce = function(e){
+    if(x>=600){
+	xdir = -1*xdir;
     }
-    else{
-	line=true;
+    if(y>=600){
+	ydir = -1*ydir;
     }
+    if(x<=0){
+	xdir = -1*xdir;
+    }
+    if(y<=0){
+	ydir = -1*ydir;
+    }
+    x += xdir;
+    y += ydir;
+    
+    draw();
+    
+    id =  window.requestAnimationFrame(bounce);
+    
 }
 
 var draw = function(e){
-    if(toggle){
-	if(x>=600){
-	    xdir = -1*xdir;
-	}
-	if(y>=600){
-	    ydir = -1*ydir;
-	}
-	if(x<=600){
-	    xdir = -1*xdir;
-	}
-	if(y<=600){
-	    ydir = -1*ydir;
-	}
-	x = x + xdir;
-	y = y + ydir;
-    }
-    else{
+	ctx.clearRect(0, 0, 600, 600);
 	ctx.beginPath();
 	ctx.arc(x, y, 10, 0, 2*Math.PI);
 	ctx.fill();
-	x+=1;
-    }
-    window.requestAnimationFrame(draw);
 }
 
+var stop = function(){
+    window.cancelAnimationFrame(id);
+}
 
-
-c.addEventListener("click", draw);
+c.addEventListener("click", bounce);
 toggled.addEventListener("click", toggler);
-stopped.addEventListener("click", clearer);
-
-/*
-fillStyle()
-strokeStyle()
-clearRect()
-fillStyle()
-*/
+stopped.addEventListener("click", stopper);
